8bd6c30bcfdd701f5861120c966ea410d6c05988
MichalPC <zfac211@rhul.ac.uk>
2020-10-28 07:11:41 +0000

    Fix incorrect varaible name.

--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -12,7 +12,7 @@ export function calculateWinner(squares)  {
         [2, 4, 6]
     ];
     for (let i = 0; i < winningLines.length; i++) {
-        const [a, b, c] = lines[i];
+        const [a, b, c] = winningLines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a];
         }
